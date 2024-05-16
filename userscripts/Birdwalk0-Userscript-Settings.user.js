01c9a6eee1d4f2952628e89fa3caddacdae89dfb
birdwalk0 <duckydee@live.com>
2024-05-16 21:13:35 +0000

    Added some more scripts

---
+++ userscripts/Birdwalk0-Userscript-Settings.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Neopets - Birdwalk0 Userscript Settings
+// @namespace    birdwalk0_neo
+// @version      2024-05-14
+// @description  try to take over the world!
+// @author       birdwalk0
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+localStorage.setItem('birdwalkConfig', JSON.stringify({
+    isAutoTripleZeroEnabled: true,
+}));
---
+++ userscripts/Wishing-Well-Filler.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Neopets - Wishing Well Filler
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  Populates the Wishing Well inputs so you don't have to
+// @author       Birdwalk
+// @match        *://*.neopets.com/wishing.phtml*
+// @grant        none
+// ==/UserScript==
+
+/* global $ */
+
+(function() {
+    // You can configure the donation amount. Note that anything below 21 NP will be ignored by the Wishing Well.
+    const myDonationAmount = 21;
+    // Update this list with any wishes you have.
+    const myWishes = [
+        "Cliffhanger Stamp",
+        "Neopets 24th Birthday Goodie Bag",
+        "Christmas Scene Stamp",
+        "Diadem of the Deep",
+    ];
+
+    $('input[name="donation"]').val(myDonationAmount);
+
+    if (!myWishes.length) return; // Stop execution if there are no wishes
+
+    // Declare a function to get a random integer that is >= 0 and < max (or just return 0 if max is 1)
+    const getRandomInt = (max) => max === 1 ? 0 : Math.floor(Math.random() * max);
+
+    // Get a random wish from the list above
+    const randomWish = myWishes[getRandomInt(myWishes.length)];
+
+    // Set the value of the "wish" input
+    $('input[name="wish"]').val(randomWish);
+})();
