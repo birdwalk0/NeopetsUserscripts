4cbcc36892a40de70b8b9f86bd00ad599dfa329d
birdwalk0 <duckydee@live.com>
2024-05-17 11:46:23 -0400

    Fixed bug in BetterInventoryDropdown that caused duplicate groups

--- userscripts/BetterInventoryDropdown.user.js
+++ userscripts/BetterInventoryDropdown.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Neopets - Better Inventory Dropdown
 // @description Organizes options in the inventory item dropdown into groups
-// @version     1.0.0
+// @version     1.0.1
 // @author      birdwalk0
 // @namespace   birdwalk0_neo
 // @icon        https://www.neopets.com/favicon.ico
@@ -18,18 +18,22 @@
     const mutationObserverConfig = { subtree: true, childList: true };
 
     /**
-     * Creates an `optgroup` node, moves provided `option` nodes into that group, and appends the group to 
+     * Creates an `optgroup` node, moves provided `option` nodes into that group, and appends the group to
      * the given `select` element. Will not create an empty `optgroup`.
      * @param {NodeList} selectNode the select node to which to add the new `optgroup`
      * @param {string} groupLabel the label for the `optgroup`
      * @param {NodeList} optionNodes option nodes to move inside the `optgroup`
-     * @returns 
      */
     function appendOptGroup(selectNode, groupLabel, optionNodes) {
         if (!optionNodes.length) return; // if are no options, do not continue
-        const optGroup = $(`<optgroup label="${groupLabel}"></optgroup>`); // Create new option group
-        optGroup.append(optionNodes); // Move nodes to the new group
-        selectNode.append(optGroup); // Append group to the bottom of the select dropdown
+        // Find the optgroup if it already exists, or create a new one
+        let optgroup = selectNode.find(`optgroup[label="${groupLabel}"]`);
+        if (!optgroup.length) {
+            // The optgroup does not yet exist, so let's create it and add it to the dropdown
+            optgroup = $(`<optgroup label="${groupLabel}"></optgroup>`);
+            selectNode.append(optgroup);
+        }
+        optgroup.append(optionNodes); // Move option nodes inside to the optgroup
     }
 
     /**
@@ -38,11 +42,13 @@
      *  - "Use" group containing "Feed to ...", "Bless ...", "Use on ...", "Equip ...", or any other options
      *      with values that contain one or more spaces
      * Any inputs that do not fall into the above categories will remain ungrouped at the top of the dropdown.
-     * @param {NodeList} selectNode The jQuery node of the select element 
+     * Only direct descendants of the `selectNode` will be evaluated, so if options have already been categorized
+     * no updates will be made.
+     * @param {NodeList} selectNode The jQuery node of the select element
      */
     function addOptGroups(selectNode) {
-        appendOptGroup(selectNode, "Toss", selectNode.find(`option[value="drop"], option[value="donate"]`));
-        appendOptGroup(selectNode, "Use", selectNode.find(`option[value*=" "]`));
+        appendOptGroup(selectNode, "Toss", selectNode.children(`option[value="drop"], option[value="donate"]`));
+        appendOptGroup(selectNode, "Use", selectNode.children(`option[value*=" "]`));
     }
 
     // Wait for the parent we want to monitor to appear on the page
@@ -58,17 +64,12 @@
              */
             (mutationList, observer) => mutationList.forEach((mutation) => {
                 if (!mutation.addedNodes.length) return; // if nodes were not added to the popup body, abort
-                
                 const select = $(mutatingParent).find(selectElementSelector);
-                
                 if (!select.length) return; // if no select elements are found, abort the function
-                
                 observer.disconnect();
                 console.debug("Disconnected observer"); // so it doesn't fire as we make changes
-                
                 addOptGroups(select);
                 console.debug("Added option groups to select dropdown, if applicable.");
-                
                 observer.observe(mutatingParent, mutationObserverConfig); // reactivate the observer
                 console.debug("Reconnected observer");
             })
